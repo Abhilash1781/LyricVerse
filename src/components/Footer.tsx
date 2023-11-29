0ba404d7b4bd0913db89301ed2e2ad0fb4b35672
Abhilash S  Bharadwaj <72621930+Abhilash1781@users.noreply.github.com>
2023-11-29 23:17:42 +0530

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable @next/next/no-img-element */
+export default function Footer() {
+  return (
+    <p className="flex gap-2 justify-center items-center absolute bottom-0 w-full p-6 font-sans text-sm text-zinc-300 md:w-2/3 md:p-4 lg:w-1/2 ">
+      Made with {`<3`} by{" "}
+      <a
+        className="text-pink-200"
+        href="https://github.com/Abhilash1781"
+        target={"_blank"}
+        rel="noreferrer"
+      >
+        Abhilash Abinash Ayush
+      </a>
+    </p>
+  );
+}
